25c4de3126e3eee46c30829d44d563ea598bb22c
Andy Ruz <86988806+Anghy73@users.noreply.github.com>
2023-06-27 02:56:47 -0400

    routes ready

--- src/index.js
+++ src/index.js
@@ -1,44 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-
-let data = [
-  {
-    "id": 1,
-    "name": "López",
-    "lastname": "Gonzáles",
-    "age": 35,
-    "country": "Chile",
-    "hobbies": [
-      "Books",
-      "Techno",
-      "Biker"
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Perez",
-    "lastname": "Cabrera",
-    "age": 29,
-    "country": "España",
-    "hobbies": [
-      "Travel",
-      "Dance",
-      "Sing"
-    ]
-  },
-  {
-    "id": 3,
-    "name": "Andy",
-    "lastname": "Ruz",
-    "age": 18,
-    "country": "Chile",
-    "hobbies": [
-      "Programming",
-      "Street workout",
-      "Games"
-    ]
-  }
-]
+import router from "./routes/index.js";
 
 const app = express();
 
@@ -50,52 +12,8 @@ app.set('nameServer', 'CRUD')
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.all("/", (req, res) => {
-  res.json({
-    "message": "Hello World"
-  })
-});
-
-app.get('/people', (req, res) => {
-  res.send(data)
-})
-
-app.get('/people/:id', (req, res) => {
-  const dataFound = data.find(person => person.id === parseInt(req.params.id))
-  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
-  res.send(dataFound)
-})
-
-app.post('/people', (req, res) => {
-  const newData = {id: data.length + 1,...req.body}
-  data.push(newData)
-  res.send(newData)
-})
-
-app.put('/people/:id', (req, res) => {
-  const newData = req.body
-  const dataFound = data.find(person => person.id === parseInt(req.params.id))
-
-  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
-
-  data = data.map(person => person.id === parseInt(req.params.id) ? {...person, ...newData} : person)
-
-  res.json({
-    message: "Person updated successfully"
-  })
-})
-
-app.delete('/people/:id', (req, res) => {
-  const dataFound = data.find(person => person.id === parseInt(req.params.id))
-
-  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
-
-  data = data.filter(person => person.id !== parseInt(req.params.id))
-
-  res.json({
-    message: "Person delete successfully"
-  })
-})
+// routing
+app.use(router)
 
 app.listen(7000, () => {
   console.log(`Server ${app.get('nameServer')} ready on port ${app.get('port')}`)
---
+++ src/routes/index.js
@@ -0,0 +1,91 @@
+import { Router } from 'express'
+
+const router = Router()
+
+let data = [
+  {
+    "id": 1,
+    "name": "López",
+    "lastname": "Gonzáles",
+    "age": 35,
+    "country": "Chile",
+    "hobbies": [
+      "Books",
+      "Techno",
+      "Biker"
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Perez",
+    "lastname": "Cabrera",
+    "age": 29,
+    "country": "España",
+    "hobbies": [
+      "Travel",
+      "Dance",
+      "Sing"
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Andy",
+    "lastname": "Ruz",
+    "age": 18,
+    "country": "Chile",
+    "hobbies": [
+      "Programming",
+      "Street workout",
+      "Games"
+    ]
+  }
+]
+
+router.all("/", (req, res) => {
+  res.json({
+    "message": "Hello World"
+  })
+});
+
+router.get('/people', (req, res) => {
+  res.send(data)
+})
+
+router.get('/people/:id', (req, res) => {
+  const dataFound = data.find(person => person.id === parseInt(req.params.id))
+  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
+  res.send(dataFound)
+})
+
+router.post('/people', (req, res) => {
+  const newData = {id: data.length + 1,...req.body}
+  data.push(newData)
+  res.send(newData)
+})
+
+router.put('/people/:id', (req, res) => {
+  const newData = req.body
+  const dataFound = data.find(person => person.id === parseInt(req.params.id))
+
+  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
+
+  data = data.map(person => person.id === parseInt(req.params.id) ? {...person, ...newData} : person)
+
+  res.json({
+    message: "Person updated successfully"
+  })
+})
+
+router.delete('/people/:id', (req, res) => {
+  const dataFound = data.find(person => person.id === parseInt(req.params.id))
+
+  if (!dataFound) return res.status(404).json({"message": "No se encontro el dato"})
+
+  data = data.filter(person => person.id !== parseInt(req.params.id))
+
+  res.json({
+    message: "Person delete successfully"
+  })
+})
+
+export default router
